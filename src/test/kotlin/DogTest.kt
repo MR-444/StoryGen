4eefff52ba12024bdd57a7aab873046534d382bc
mr-444 <markus_reinke@gmx.net>
2023-07-05 06:26:42 +0200

    Added more testcases (to test github pilot)

--- src/test/kotlin/DogTest.kt
+++ src/test/kotlin/DogTest.kt
@@ -4,6 +4,8 @@ import domain.location.LocationFactory
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
+
+
 class DogTest : FunSpec({
     // initialize
     val playGround = LocationFactory().create(Location.PlayGround)
@@ -36,4 +38,33 @@ class DogTest : FunSpec({
     test("toString() should be correct.") {
         fido.toString() shouldBe "domain.livingThing.Dog(name=Fido, health=100, location=Playground, height=21.0)"
     }
+
+    test("bark() should be correct.") {
+        fido.bark shouldBe "Woof!"
+    }
+
+    test("healthToLiteral() should be correct.") {
+        fido.healthToLiteral() shouldBe "healthy"
+    }
+
+    test("lookAround() should be correct.") {
+        fido.lookAround() shouldBe "Fido looks around."
+    }
+
+    test("say() should be correct.") {
+        fido.say("Hello") shouldBe "Fido says: Hello"
+    }
+
+    test("moveToAndBack() should be correct.") {
+        fido.moveToAndBack(pond, null) shouldBe "Fido runs to the Pond."
+    }
+
+    // test if the complete class is correct
+    test("Dog should be correct.") {
+        fido.name shouldBe "Fido"
+        fido.health shouldBe 100
+        fido.location shouldBe playGround
+        fido.height shouldBe 21.0
+    }
+
 })
